7c78dce5d1382bf530878a39b576abe7349a672c
jgibson37 <jason.gibson15@yahoo.com>
2020-09-10 12:42:40 -0400

    added a simple range test

--- src/src/test/java/GeneratorBodyTest.java
+++ src/src/test/java/GeneratorBodyTest.java
@@ -1,9 +1,12 @@
 import org.junit.Test;
 
+import java.util.List;
+
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class GeneratorBodyTest {
+
     @Test
     public void checkPrimeNumber7901(){
         GeneratorBody primeTest = new GeneratorBody();
@@ -15,5 +18,12 @@ public class GeneratorBodyTest {
         GeneratorBody primeTest = new GeneratorBody();
         boolean result = primeTest.isPrime(7903);
         assertFalse(result);
+    }
+    @Test
+    public void checkRangeForPrime(){
+        GeneratorBody primeTest = new GeneratorBody();
+        List<Integer> result;
+        result = primeTest.generate(7900, 7920);
+        assertTrue(result.contains(7901));
     }
 }
