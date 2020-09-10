c5a2b4f22d15f141934d194b1c45b4bca903ba41
jgibson37 <jason.gibson15@yahoo.com>
2020-09-10 12:53:11 -0400

    Wrote code to pass checkRangeForPrime test Created for loop to check value range added boolean to run isPrime method to check for prime if statement to check if isPrime returned true. To cover true cases I created an arrayList to save the value to, and wrote a method that saves to the array.

--- src/src/main/java/GeneratorBody.java
+++ src/src/main/java/GeneratorBody.java
@@ -1,15 +1,28 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class GeneratorBody implements PrimeNumberGenerator {
 
+    List<Integer> primes = new ArrayList<>();
+
     @Override
     public List<Integer> generate(int startingValue, int endingValue) {
-        return null;
+        for (int x = startingValue; x <= endingValue; x++){
+            boolean result = isPrime(x);
+            if(result==true){
+                savePrime(x);
+            }
+        }
+        return primes;
+    }
+
+    private void savePrime(int x) {
+        primes.add(x);
     }
 
     @Override
     public boolean isPrime(int value) {
-        for (int i=2; i < value/2; i++){
+        for (int i = 2; i < value/2; i++){
             if (value % i == 0){
                 return false;
             }
