d781498baa6b15ada625e1b8477e3387259ca11f
jgibson37 <jason.gibson15@yahoo.com>
2020-09-10 12:38:22 -0400

    added test to check non prime number not passing through

--- src/src/test/java/GeneratorBodyTest.java
+++ src/src/test/java/GeneratorBodyTest.java
@@ -1,5 +1,6 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class GeneratorBodyTest {
@@ -8,5 +9,11 @@ public class GeneratorBodyTest {
         GeneratorBody primeTest = new GeneratorBody();
         boolean result = primeTest.isPrime(7901);
         assertTrue(result);
+    }
+    @Test
+    public void checkNotPrimeNumber7903(){
+        GeneratorBody primeTest = new GeneratorBody();
+        boolean result = primeTest.isPrime(7903);
+        assertFalse(result);
     }
 }
