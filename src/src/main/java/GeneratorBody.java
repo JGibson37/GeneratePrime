b53bcae82142c33aeebbf7289601485342e006ba
jgibson37 <jason.gibson15@yahoo.com>
2020-09-10 12:25:36 -0400

    initial project setup Added Gradle to create test suite Created GeneratorBody Created GeneratorBodyTest Created PrimeNumberGenerator Created GeneratePrimeApp Implemented interface parameters, autogenerated methods in GeneratorBody

---
+++ src/src/main/java/GeneratorBody.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class GeneratorBody implements PrimeNumberGenerator {
+
+    @Override
+    public List<Integer> generate(int startingValue, int endingValue) {
+        return null;
+    }
+
+    @Override
+    public boolean isPrime(int value) {
+        return false;
+    }
+}
